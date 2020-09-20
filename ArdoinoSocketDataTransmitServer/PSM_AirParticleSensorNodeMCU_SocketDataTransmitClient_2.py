c121e090031a83ba463bf578e58324b00a89baaa
unknown <fengziyuan72@gmail.com>
2020-09-21 00:38:29 +0800

    0.2.3 (With function that read and refresh form the object)

--- ArdoinoSocketDataTransmitServer/PSM_AirParticleSensorNodeMCU_SocketDataTransmitClient_2.py
+++ ArdoinoSocketDataTransmitServer/PSM_AirParticleSensorNodeMCU_SocketDataTransmitClient_2.py
@@ -16,9 +16,21 @@ def hexShowNew(argv):
 
 
 class PSMconnection:
-    def __init__(self,PSMNetworkConfig,sock):
+    sock=0
+    pm1point0 = 0
+    pm2point5 = 0
+    pm10 = 0
+    pm1point0_air = 0
+    pm2point5_air = 0
+    pm10_air = 0
+    over0point3um = 0
+    over0point5um = 0
+    over1point0um = 0
+    over2point5um = 0
+    over5point0um = 0
+    over10um = 0
+    def __init__(self,PSMNetworkConfig):
         self.PSMNetworkConfig = PSMNetworkConfig
-        self.sock = sock
     
     def PSMsocket_start(self):
         self.sock = socket.socket()
@@ -44,6 +56,25 @@ class PSMconnection:
         else:
             return 'SUM Authentication failed'
 
-PSM = PSMconnection(PSMNetworkConfig=('192.168.31.230',8080),sock=0)
+    def RefreshData(self,):
+        LisDataDict = self.getrawData()
+        self.m1point0      =  int(LisDataDict[4]  + LisDataDict[5] ,16)
+        self.pm2point5     =  int(LisDataDict[6]  + LisDataDict[7] ,16)
+        self.pm10          =  int(LisDataDict[8]  + LisDataDict[9] ,16)
+        self.pm1point0_air =  int(LisDataDict[10] + LisDataDict[11],16)
+        self.pm2point5_air =  int(LisDataDict[12] + LisDataDict[13],16)
+        self.pm10_air      =  int(LisDataDict[14] + LisDataDict[15],16)
+        self.um0point3     =  int(LisDataDict[16] + LisDataDict[17],16)
+        self.um0point5     =  int(LisDataDict[18] + LisDataDict[19],16)
+        self.um1point0     =  int(LisDataDict[20] + LisDataDict[21],16)
+        self.um2point5     =  int(LisDataDict[22] + LisDataDict[23],16)
+        self.um5point0     =  int(LisDataDict[24] + LisDataDict[25],16)
+        self.um10          =  int(LisDataDict[26] + LisDataDict[27],16)
+        
+        
+
+PSM = PSMconnection(PSMNetworkConfig=('192.168.31.230',8080))
 PSM.PSMsocket_start()
-print(PSM.getrawData())
+while True:
+    PSM.RefreshData()
+    print(PSM.pm2point5_air)
